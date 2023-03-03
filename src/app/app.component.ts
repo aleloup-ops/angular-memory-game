64de322c6314421768436629455fcaa6ef7e1066
Alexis Leloup <alexis.leloup@epitech.eu>
2023-03-03 09:51:47 +0100

    Adding dark mode / Changing card covers

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,10 +8,27 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'memory-game-angular';
 
+  private isDarkTheme: boolean = false;
+  public darkThemeIcon: string = 'https://cdn-icons-png.flaticon.com/512/581/581601.png';
+
   public pokemonTeam: Array<any> = [];
 
   public retrieveData($event: any): void {
     console.log($event);
     this.pokemonTeam = $event;
+  }
+
+  public toggleDarkMode(): void {
+    if (!this.isDarkTheme) {
+      var root = document.documentElement;
+      root.className += ' dark';
+      this.isDarkTheme = true;
+      this.darkThemeIcon = 'https://cdn-icons-png.flaticon.com/512/2917/2917242.png';
+    } else {
+      var root = document.documentElement;
+      root.classList.remove('dark');
+      this.isDarkTheme = false;
+      this.darkThemeIcon = 'https://cdn-icons-png.flaticon.com/512/581/581601.png';
+    }
   }
 }
